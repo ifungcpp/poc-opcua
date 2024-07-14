bc7fd2eebc3857c6ab90311ca1298a0fbfa25f58
Harry FUNG <aeekhfung@gmail.com>
2024-07-14 14:30:16 +0800

    add open62541 client*.c

--- src/S2OPC/build_and_run.sh
+++ src/S2OPC/build_and_run.sh
@@ -2,14 +2,30 @@
 
 set -e
 
+let_run()
+{
+    echo
+    echo $*
+    echo
+    time $*
+}
+
+let_tcpdump()
+{
+    (let_run tcpdump -i any -n 'tcp port 4840' -w capture.pcap) &
+}
+
 let_make()
 {
-    make clean
-    make
+    let_run make clean
+    let_run make
 }
 
 let_subscribe()
 {
+    echo
+    echo SET_SUBSCRIBE_TIMEOUT=yes ./subscribe.x i=2258
+    echo
     SET_SUBSCRIBE_TIMEOUT=yes ./subscribe.x i=2258
 }
 
@@ -17,8 +33,9 @@ main_func()
 {
     local cur_dir=$(realpath $(dirname $0))
     pushd $cur_dir >/dev/null
+    let_tcpdump
     let_make
-    for i in {1..19}
+    for i in {1..9}
     do
         echo wait for opc ua server start up ... $i
         sleep 1
